83d5613f0fcbc3524cb2245f75048a0b3239deef
Jens Andre Kålen <ja.kaalen@gmail.com>
2016-11-13 10:43:49 +0100

    yorro

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@ var gulp = require('gulp');
 var webpackConfig = require('./webpack.config.js');
 var plumber = require('gulp-plumber');
 
-gulp.task('default', function(){
-    return gulp.src('.')
-  .pipe(webpack(webpackConfig))
-  .pipe(gulp.dest('.'));
+gulp.task('default', function () {
+  return gulp.src('.')
+    .pipe(plumber())
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest('.'));
 });
